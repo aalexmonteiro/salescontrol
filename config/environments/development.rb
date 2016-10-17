590dbca159f4b8491874d3a173ebfde909d315d4
Alex <aalexmonteiro@gmail.com>
2016-10-17 01:01:56 -0300

    Adjusting reset password and icons

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,20 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  config.action_mailer.default_url_options = {:host => '127.0.0.1',:port => 3000}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: :login,
+    enable_starttls_auto: true,
+    user_name: ENV["USER"],
+    password: ENV["PASSWORD"]
+  }
+
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
